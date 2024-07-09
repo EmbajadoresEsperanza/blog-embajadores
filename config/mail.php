7083e1febee9667589336ad93f7dfc7b0e6c40c8
ecasallas07 <ecasallas07@gmail.com>
2024-07-08 20:15:42 -0500

    first commit

--- config/mail.php
+++ config/mail.php
@@ -98,7 +98,7 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
+        'address' => env('MAIL_FROM_ADDRESS', 'ecasallas07@gmail.com'),
         'name' => env('MAIL_FROM_NAME', 'Example'),
     ],
 
