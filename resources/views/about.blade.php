469c3b7be495dccb068cd9a97a7ee304ee502072
ecasallas07 <ecasallas07@gmail.com>
2024-07-12 16:17:25 -0500

    feat: solution problem permisions of files and customize

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,18 +1,18 @@
 <x-app-layout meta-title="TheCodeholic Blog - About us">
 
-    <div class="container mx-auto flex flex-wrap py-6">
+    <div class="container flex flex-wrap py-6 mx-auto">
 
         <!-- Post Section -->
-        <section class="w-full md:w-full flex flex-col items-center px-3">
+        <section class="flex flex-col items-center w-full px-3 md:w-full">
 
-            <article class="flex flex-col shadow my-4">
-                @if($widget && $widget->image)
+            <article class="flex flex-col my-4 shadow">
+                @if ($widget && $widget->image)
                     <img src="/storage/{{ $widget->image }}">
                 @endif
 
-                <div class="bg-white flex flex-col justify-start p-6">
-                    <h1 class="text-3xl font-bold hover:text-gray-700 pb-4">
-                        {{$widget ? $widget->title : ''}}
+                <div class="flex flex-col justify-start p-6 bg-white">
+                    <h1 class="pb-4 text-3xl font-bold hover:text-gray-700">
+                        {{ $widget ? $widget->title : '' }}
                     </h1>
                     <div>
                         {!! $widget ? $widget->content : '' !!}
--- resources/views/post/view.blade.php
+++ resources/views/post/view.blade.php
@@ -1,67 +1,67 @@
 <x-app-layout :meta-title="$post->meta_title ?: $post->title" :meta-description="$post->meta_description">
     <div class="flex">
         <!-- Post Section -->
-        <section class="w-full md:w-2/3 flex flex-col px-3">
-
-            <article class="flex flex-col shadow my-4">
+        <section class="flex flex-col w-full px-3 md:w-2/3">
+            <article class="flex flex-col my-4 shadow">
                 <!-- Article Image -->
                 <a href="#" class="hover:opacity-75">
-                    <img src="{{$post->getThumbnail()}}">
+                    <img src="{{ $post->getThumbnail() }}">
                 </a>
-                <div class="bg-white flex flex-col justify-start p-6">
+                <div class="flex flex-col justify-start p-6 bg-white">
                     <div class="flex gap-4">
-                        @foreach($post->categories as $category)
-                            <a href="#" class="text-blue-700 text-sm font-bold uppercase pb-4">
-                                {{$category->title}}
+                        @foreach ($post->categories as $category)
+                            <a href="#" class="pb-4 text-sm font-bold uppercase" style="color:orange !important;">
+                                {{ $category->title }}
                             </a>
                         @endforeach
                     </div>
-                    <h1 class="text-3xl font-bold hover:text-gray-700 pb-4">
-                        {{$post->title}}
+                    <h1 class="pb-4 text-3xl font-bold hover:text-gray-700">
+                        {{ $post->title }}
                     </h1>
-                    <p href="#" class="text-sm pb-8">
-                        By <a href="#" class="font-semibold hover:text-gray-800">{{$post->user->name}}</a>, Published on
-                        {{$post->getFormattedDate()}} | {{ $post->human_read_time }}
+                    <p href="#" class="pb-8 text-sm">
+                        By <a href="#" class="font-semibold hover:text-gray-800">{{ $post->user->name }}</a>,
+                        Published on
+                        {{ $post->getFormattedDate() }} | {{ $post->human_read_time }}
                     </p>
                     <div>
                         {!! $post->body !!}
                     </div>
 
-                    <livewire:upvote-downvote :post="$post"/>
+                    <livewire:upvote-downvote :post="$post" />
                 </div>
             </article>
 
-            <div class="w-full flex pt-6">
+            <div class="flex w-full pt-6">
                 <div class="w-1/2">
-                    @if($prev)
-                        <a href="{{route('view', $prev)}}"
-                           class="block w-full bg-white shadow hover:shadow-md text-left p-6">
-                            <p class="text-lg text-blue-800 font-bold flex items-center">
-                                <i class="fas fa-arrow-left pr-1"></i>
+                    @if ($prev)
+                        <a href="{{ route('view', $prev) }}"
+                            class="block w-full p-6 text-left bg-white shadow hover:shadow-md">
+                            <p class="flex items-center text-lg font-bold text-blue-800">
+                                <i class="pr-1 fas fa-arrow-left"></i>
                                 Previous
                             </p>
-                            <p class="pt-2">{{\Illuminate\Support\Str::words($prev->title, 5)}}</p>
+                            <p class="pt-2">{{ \Illuminate\Support\Str::words($prev->title, 5) }}</p>
                         </a>
                     @endif
                 </div>
                 <div class="w-1/2">
-                    @if($next)
-                        <a href="{{route('view', $next)}}"
-                           class="block w-full bg-white shadow hover:shadow-md text-right p-6">
-                            <p class="text-lg text-blue-800 font-bold flex items-center justify-end">Next
-                                <i
-                                    class="fas fa-arrow-right pl-1"></i></p>
+                    @if ($next)
+                        <a href="{{ route('view', $next) }}"
+                            class="block w-full p-6 text-right bg-white shadow hover:shadow-md">
+                            <p class="flex items-center justify-end text-lg font-bold text-blue-800">Next
+                                <i class="pl-1 fas fa-arrow-right"></i>
+                            </p>
                             <p class="pt-2">
-                                {{\Illuminate\Support\Str::words($next->title, 5)}}
+                                {{ \Illuminate\Support\Str::words($next->title, 5) }}
                             </p>
                         </a>
                     @endif
                 </div>
             </div>
 
-            <livewire:comments :post="$post"/>
+            <livewire:comments :post="$post" />
         </section>
 
-        <x-sidebar/>
+        <x-sidebar />
     </div>
 </x-app-layout>
